813d1c1e012120d18f16714fd9264d3434c02644
FranckLdx <franck.ledoux.pro@gmail.com>
2016-10-31 15:55:00 +0100

    First example

---
+++ example/index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const uploaderExpress = require('../index.js'); //require('uploaderExpress');
+const uploaderMiddleware = uploaderExpress.middleware({
+  maxSize: '1gb',
+  uploadDir: './upload',
+  type: 'json'
+});
+
+app.post('/upload', uploaderMiddleware, (req, res, next) => {
+  res.status(200).json({
+    file:req.x_file.name,
+    size:req.x_file.size
+  });
+});
+
+app.use(function(err, req, res, next) {
+  console.log(`Error while processing a request. Request: `, {originalUrl:req.originalUrl, headers: req.headers}, '\nError: ', err);
+  if (err.status) {
+    res.status(err.status);
+    if (err.message) {
+      res.end(err.message);
+    } else {
+      res.end();
+    }
+  } else {
+    res.status(500).end('Something goes wrong!');
+  }
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
